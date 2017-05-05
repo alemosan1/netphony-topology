b38208ce2c591bdef86da5f1ef4277d9f444646e
b-jsm <javier.solermacias@telefonica.com>
2017-05-05 12:38:17 +0200

    Bug fix node-edge name

--- src/main/java/es/tid/topologyModuleBase/TAPITopoModel/model/TranslateModel.java
+++ src/main/java/es/tid/topologyModuleBase/TAPITopoModel/model/TranslateModel.java
@@ -13,9 +13,6 @@ import java.util.UUID;
 import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.Map;
-//import es.tid.tedb.TE_Information;
-//import es.tid.tedb.FileTEDBUpdater;
-//import es.tid.tedb.TEDB;
 
 
 
@@ -25,22 +22,14 @@ public class TranslateModel {
 	// Variable for testing
 	private static boolean printingLink = true;
 	private static boolean printingNode = true;
-	private static NameAndValue nameL = new NameAndValue();
-	private static NameAndValue labelL = new NameAndValue();
-	private static AdminStatePac stateL = new AdminStatePac();
-	private static NameAndValue labelN = new NameAndValue();
-	private static AdminStatePac stateN = new AdminStatePac();
 	
-
-	
-	// Hashtable of key:value -> nodeIP:nodeUUID
+	// Hashtable of key:value ->	 nodeIP:nodeUUID
 	private static Hashtable<String,String> nodes_uuid = new Hashtable<String,String>();
 	// Hashtable of key:value -> nodeIP/portNumber:portUUID
 	private static Hashtable<String,String> ports_uuid = new Hashtable<String,String>();
-	// Hashtable mapping nodes and ports, of key:value -> portUUID:nodeIP
-	private static Hashtable<String,String> ports_nodes = new Hashtable<String,String>();
-	// Hashtable mapping nodes and ports, of key:value -> nodeIP:listOfPorts
-//	private static Hashtable<String,List<NodeEdgePoint>> nodes_ports = new Hashtable<String,List<NodeEdgePoint>>();
+	// Hashtable mapping nodes and ports, of key:value -> NodeAndEdges(nodeIP,port):portUUID
+	private static Hashtable<NodeAndEdges,String> nodes_ports = new Hashtable<NodeAndEdges,String>();
+	
 	
 	public static Topology translateTopology(String topId, DomainTEDB ted){
 		Topology topo = new Topology();
@@ -55,9 +44,6 @@ public class TranslateModel {
 		topo.getName();
 		topo.getLabel();
 		topo.getLayerProtocolName();
-//		if (topo.getName().length == 0) {
-//			NameAndValue = 
-//		}
 		
 		  for(IntraDomainEdge link : ted.getIntraDomainLinks()){
 			  String src_L = ((java.net.Inet4Address)link.getSource()).getHostAddress();
@@ -70,14 +56,19 @@ public class TranslateModel {
 			  ports_uuid.putIfAbsent(srcEdge, UUID_generate());
 			  ports_uuid.putIfAbsent(dstEdge, UUID_generate());
 			  
-			  ports_nodes.putIfAbsent(ports_uuid.get(srcEdge), src_L);
-			  ports_nodes.putIfAbsent(ports_uuid.get(dstEdge), dst_L);
-//			  ports_nodes.putIfAbsent(src_L, nodeedge...)
-//			  ports_nodes.putIfAbsent(dst_L, nodeedge...)
+			  NodeAndEdges src_nodeEdges = new NodeAndEdges(src_L, link.getSrc_if_id());
+			  NodeAndEdges dst_nodeEdges = new NodeAndEdges(dst_L, link.getDst_if_id());
+			  nodes_ports.putIfAbsent(src_nodeEdges, ports_uuid.get(srcEdge));
+			  nodes_ports.putIfAbsent(dst_nodeEdges, ports_uuid.get(dstEdge));
 			  
 			  topo.getLink().add(translateEdge(ted, link));
 		  }
-//		  System.out.println(nodes_uuid.toString());
+		  /*
+		  FOR TESTING nodes_ports HASHTABLE (SEE KEYS:VALUES)
+		  for(Map.Entry entry: nodes_ports.entrySet()) {
+			  System.out.println("NODE_IP = " + ((NodeAndEdges)entry.getKey()).getNodeIP() + " PORT = " + ((NodeAndEdges)entry.getKey()).getPort() + " UUID = " + nodes_ports.get((NodeAndEdges)entry.getKey()));
+		  }
+		  */
 		  
 		  List<Node> nodes = new ArrayList<Node>();
 		  for(Object node : ted.getIntraDomainLinksvertexSet()){
@@ -96,52 +87,63 @@ public class TranslateModel {
 	public static Link translateEdge(DomainTEDB ted, IntraDomainEdge e){
 		Link link = new Link();
 		
-		// Set name, label and state
-//		intializeLinkTest(link);
-		
-		link.getDirection();
-		if (link.getDirection() == null) {
-			link.setDirection(Link.DirectionEnum.UNDEFINED_OR_UNKNOWN);
-		}
-		link.getLabel();
-		link.getLayerProtocolName();
-		link.getLpTransition();
-		
-		String src_L = ((java.net.Inet4Address)e.getSource()).getHostAddress();// + " with IP: " +((java.net.Inet4Address)e.getSource()).getHostAddress());
-		String dst_L = ((java.net.Inet4Address)e.getTarget()).getHostAddress();// + " with IP: " +((java.net.Inet4Address)e.getTarget()).getHostAddress());
-		link.addNodeItem(nodes_uuid.get(src_L));
-		link.addNodeItem(nodes_uuid.get(dst_L));
-		/*for (int i=0; i < link.getNode().size(); i++) {
-			System.out.println("NODOS_L_"+i+" = " +link.getNode().get(i));
-			if (i == link.getNode().size()-1) {System.out.println("\n");}
-		}*/
-		
-		link.getName();
-		
-		String srcEdge = src_L + "/" + Long.toString(e.getSrc_if_id()).toString();
-		srcEdge += "/" + ports_uuid.get(srcEdge);
-		link.getNodeEdgePoint().add(srcEdge);
-		String dstEdge = dst_L + "/" + Long.toString(e.getDst_if_id()).toString();
-		dstEdge += "/" + ports_uuid.get(dstEdge);
-		link.getNodeEdgePoint().add(dstEdge);
-		
-		link.getRiskParameter();
-		link.getState();
-		link.getTransferCapacity();
-        link.getTransferCost();
-		link.getTransferIntegrity();
-		link.getTransferTiming();
 		link.getUuid();
 		if (link.getUuid() == null) {
 			link.setUuid(UUID_generate());
 		}
-		link.getValidation();
 		
+		link.getName();
+		
+		link.getLabel();
+		
+		String src_L = ((java.net.Inet4Address)e.getSource()).getHostAddress();
+		String dst_L = ((java.net.Inet4Address)e.getTarget()).getHostAddress();
+		link.addNodeItem(nodes_uuid.get(src_L));
+		link.addNodeItem(nodes_uuid.get(dst_L));
+		/*
+		FOR TESTING LIST OF NODES
+	 	for (int i=0; i < link.getNode().size(); i++) {
+			System.out.println("NODOS_L_"+i+" = " +link.getNode().get(i));
+			if (i == link.getNode().size()-1) {System.out.println("\n");}
+		}*/
+		
+		// Every src/dst port (node-edge-point) as nodeUUID/portUUID
+		String srcEdge = nodes_uuid.get(src_L) + "/" + ports_uuid.get(src_L + "/" + Long.toString(e.getSrc_if_id()).toString());
+		link.getNodeEdgePoint().add(srcEdge);
+		String dstEdge = nodes_uuid.get(dst_L) + "/" + ports_uuid.get(dst_L + "/" + Long.toString(e.getDst_if_id()).toString());
+		link.getNodeEdgePoint().add(dstEdge);
+		
+		link.getState();
+		AdminStatePac stateL = new AdminStatePac();
+		if (link.getState() == null) {
+			stateL.setAdministrativeState(AdminStatePac.AdministrativeStateEnum.UNLOCKED);
+			stateL.setLifecycleState(AdminStatePac.LifecycleStateEnum.INSTALLED);		
+			stateL.setOperationalState(AdminStatePac.OperationalStateEnum.ENABLED);
+			link.setState(stateL);
+		}
+		
+		link.getTransferCapacity();
+        link.getTransferCost();
+		link.getTransferIntegrity();
+		link.getTransferTiming();
+		
+		link.getRiskParameter();
+		link.getValidation();
+		link.getLpTransition();
+		link.getLayerProtocolName();
+		
+		link.getDirection();
+		if (link.getDirection() == null) {
+			link.setDirection(Link.DirectionEnum.BIDIRECTIONAL);
+		}
+		
+		
+		// FOR TESTING (PRINTING IN MAIN CONSOLE)
 		if (printingLink) {
 //			System.out.println("Direction = " + link.getDirection());
-			System.out.println("EDGE = "+link.getNodeEdgePoint());
+//			System.out.println("EDGE = " + link.getNodeEdgePoint());
 //			System.out.println("LINK = " + link.toString());	
-//			printingLink = false;
+			printingLink = false;
 		}
 		
 		
@@ -236,97 +238,102 @@ public class TranslateModel {
 		
 		System.out.println("DEBUG translateNodeIP, nodeInfo (of node "+n+"):"+nodeInfo);
 		
+		node.getUuid();
 		if (node.getUuid() == null) {
 			node.setUuid(nodes_uuid.get(n.getHostAddress())); // Get UUID from hastable
 		}
 		
-		List<String> key = new ArrayList<String>();
-		String value = n.getHostAddress();
-		for(Map.Entry entry: ports_nodes.entrySet()){
-			if(value.equals(entry.getValue())){
-				key.add(entry.getKey().toString());// = entry.getKey().toString();
-//				break; //breaking because its one to one map
-			}
-		}
-//		System.out.println("Got key (portUUID) from value (nodeIP) in hashtable key:  "+ key +" value: " + value);
-		
-		for (int i=0; i < key.size(); i++) {
-			System.out.println("PORTS_UUID"+i+" = " +key.get(i) +" NODE: " + value);
-			if (i == key.size()-1) {System.out.println("\n");}
-		}
-		
-		
-		// Hashtable mapping nodes and ports, of key:value -> portUUID:nodeIP
-//		private static Hashtable<String,String> ports_nodes = new Hashtable<String,String>();
-//		ports_nodes.putIfAbsent(ports_uuid.get(srcEdge), src_L)
-		
-//		System.out.println("MAPPING = " +ports_nodes.get(n.getHostAddress()) + " IP = "+n.getHostAddress());
-//		
-//		String key = null;
-//        String value = node.getUuid();
-//        for(Map.Entry entry: nodes_uuid.entrySet()){
-//            if(value.equals(entry.getValue())){
-//                key = entry.getKey().toString();
-//                break; //breaking because its one to one map
-//            }
-//        }
-//        System.out.println("Got key from value in hashtable key:  "+ key +" value: " + value);
-		
-		
-		NodeEdgePoint e = new NodeEdgePoint();
-		node.getOwnedNodeEdgePoint().add(e);
-//		if (e.getUuid() == null) {
-//			e.setUuid(ports_uuid.get(n.getHostAddress()));
-//		}
-		
-		node.getLayerProtocolName();
-		
-		// For naming nodes and node-edge-points
+		node.getName();
+		// For naming nodes
 		NameAndValue nameN = new NameAndValue();
-		NameAndValue nameNE = new NameAndValue();
-		
 		if (node.getName().size() == 0) {
 			nameN.setValue(n.getHostAddress().toString());
 			nameN.setValueName("name");
 			node.addNameItem(nameN);
 			node.setName(node.getName());
 		}
-		node.getName();
-		node.getLabel();
-//		if (node.getLayerProtocolName() == null) {
-//			node.setLayerProtocolName(Node.LayerProtocolNameEnum.UNDEFINED_OR_UNKNOWN);
-//		}
 		
-		if (printingNode) {
-//			System.out.println("NODE = " +node.toString());
-//			printingNode = false;
-			System.out.println(key + "\n");
-			System.out.println("-----END OF NODE-----");
+		node.getLabel();
+		
+		// Add nodeIP/port as name for each port (owned-node-edge-point)
+		for(Map.Entry entry: nodes_ports.entrySet()) {
+			NodeEdgePoint e = new NodeEdgePoint();
+			// For naming nodes and node-edge-points
+			NameAndValue nameNE = new NameAndValue();
+			
+			String nodeIP = ((NodeAndEdges)entry.getKey()).getNodeIP();
+			String port = Long.toString(((NodeAndEdges)entry.getKey()).getPort()).toString();
+			if ( nodeIP.equals(n.getHostAddress()) ) {
+				node.getOwnedNodeEdgePoint().add(e);
+				
+				e.getUuid();
+				if (e.getUuid() == null) {
+					e.setUuid(nodes_ports.get((NodeAndEdges)entry.getKey()));
+				}
+				
+				e.getName();
+				if (e.getName().size() == 0) {
+					nameNE.setValue(nodeIP + "/" + port);
+					nameNE.setValueName("name");
+					e.addNameItem(nameNE);
+					e.setName(e.getName());
+				}
+				
+				e.getLabel();
+				e.getLayerProtocol();
+				e.getAggregatedNodeEdgePoint();
+				e.getMappedServiceInterfacePoint();
+				e.getState();
+				
+				e.getTerminationDirection();
+				if (e.getTerminationDirection() == null) {
+					e.setTerminationDirection(NodeEdgePoint.TerminationDirectionEnum.BIDIRECTIONAL);
+				}
+				
+				e.getLinkPortDirection();
+				if (e.getLinkPortDirection() == null) {
+					e.setLinkPortDirection(NodeEdgePoint.LinkPortDirectionEnum.BIDIRECTIONAL);
+				}
+				
+				e.getLinkPortRole();
+				if (e.getLinkPortRole() == null) {
+					e.setLinkPortRole(NodeEdgePoint.LinkPortRoleEnum.ROOT);
+				}
+			}
+			// FOR TESTING RECOVERY OF nodeIP/port, AND UUID OF THAT PORT
+//			System.out.println("NODE_IP = " + ((NodeAndEdges)entry.getKey()).getNodeIP() + " PORT = " + ((NodeAndEdges)entry.getKey()).getPort() + " UUID = " + nodes_ports.get((NodeAndEdges)entry.getKey()));
 		}
 		
+		node.getAggregatedNodeEdgePoint();
 		
-//		System.out.println("NODES 1 = " +ted.getNodeTable());
-//		System.out.println("NODES 2 = " +ted.getNodeTable().get(node));
+		node.getEncapTopology();
+//		String topoUUID = Topology.getUuid();
+		if (node.getEncapTopology() == null) {
+			node.setEncapTopology(ted.getDomainID().getHostAddress());
+		}
 		
-//		System.out.println("NODES = " + FileTEDBUpdater.graph.vertexSet());
-//		System.out.println("LABELS = "+TE_Information.getAvailableLabels());
-//		System.out.println("TOPOLOGY = "+TEDB.printTopology());
+		node.getState();
+		// For adding default state
+		AdminStatePac stateN = new AdminStatePac();
+		if (node.getState() == null) {
+			stateN.setAdministrativeState(AdminStatePac.AdministrativeStateEnum.UNLOCKED);
+			stateN.setLifecycleState(AdminStatePac.LifecycleStateEnum.INSTALLED);		
+			stateN.setOperationalState(AdminStatePac.OperationalStateEnum.ENABLED);
+			node.setState(stateN);
+		}
 		
+		node.getTransferCapacity();
+        node.getTransferCost();
+        node.getTransferIntegrity();
+        node.getTransferTiming();
+        node.getLayerProtocolName();
 		
-//		System.out.println("NODE_NAME = "+nodeInfo.getName());
-//		System.out.println("IP_ADDR = "+nodeInfo.getIpv4Address());
-//		System.out.println("NODE_SID = "+nodeInfo.getSid());
-		
-//		System.out.println("IP = " + ((Node_Info)node).getName());
-		
-//		System.out.println("Node state = "+e.getState().toString());
-//		System.out.println("n = " + n.toString());
-//		System.out.println("aggregating = " + node.getAggregatedNodeEdgePoint().add(n.toString()));
-//		System.out.println("aggregated = " + node.getAggregatedNodeEdgePoint());
-//		System.out.println("clientNode = " + e.getClientNodeEdgePoint());
-//		System.out.println("EncapTopology = " + node.getEncapTopology());
-		//e.
-		//owned-node-edge-point
+        // FOR TESTING (PRINTING IN MAIN CONSOLE)
+		if (printingNode) {
+//			System.out.println("NODE = " +node.toString());
+			printingNode = false;
+//			System.out.println("-----END OF NODE-----");
+		}
 		
 //		node.setName(n.getHostAddress());
 //		node.setNodeId(n.getHostAddress());
@@ -347,74 +354,28 @@ public class TranslateModel {
 	 */
 	public static synchronized String UUID_generate() {
 		String uuid = UUID.randomUUID().toString();
-		if ( nodes_uuid.containsValue(uuid) ) {
+		if ( nodes_uuid.containsValue(uuid) || ports_uuid.containsValue(uuid) ) {
 			uuid = UUID.randomUUID().toString();
 		}
 		return uuid;
 		
-/*		Random r = new Random();
-		
-		System.out.println("lastUUID_1 = "+lastUUID);
-		
-		String UUIDstr = "";
-		String alphabet = "abcdefABCDEF0123456789";
-		if (lastUUID == null) {
-		    for (int i = 0; i < 36; i++) {
-		    	if (i == 8 || i == 13 || i == 18 || i == 23) {
-		    		UUIDstr += "-";
-		    	} else {
-		    		UUIDstr += alphabet.charAt(r.nextInt(alphabet.length()));
-		    	}
-		    }
-		    lastUUID = UUIDstr;
-		} else {
-			System.out.println("lastUUID no null, sumo 1");
-		}
-//	    lastUUID = UUID;
-	    System.out.println("lastUUID_2 = "+lastUUID);
-	    return UUIDstr;
-*/
 	}
-	
-	private static synchronized void intializeLinkTest(Link link) {
-		/*if (link.getName().size() == 0) {
-			nameL.setValue("DEFAULT_NAME");
-			nameL.setValueName("name");
-			link.addNameItem(nameL);
-			link.setName(link.getName());
-		}*/
-		
-		if (link.getLabel().size() == 0) {
-			labelL.setValue("DEFAULT_LABEL_NAME");
-			labelL.setValueName("name");
-			link.addLabelItem(labelL);
-			link.setLabel(link.getLabel());
-		}
-		
-		if (link.getState() == null) {
-			AdminStatePac stateL = new AdminStatePac();
-			stateL.setAdministrativeState(AdminStatePac.AdministrativeStateEnum.fromValue("null")/*UNLOCKED*/);
-			stateL.setLifecycleState(AdminStatePac.LifecycleStateEnum.fromValue("null")/*INSTALLED*/);		
-			stateL.setOperationalState(AdminStatePac.OperationalStateEnum.fromValue("null")/*ENABLED*/);
-			link.setState(stateL);
-		}
-
-	}
-	
 	
 	/*
-	 * System.out.println("MAPPING = " +nodes_uuid.get(n.getHostAddress()) + " IP = "+n.getHostAddress());
+	FOR MAKING INVERSE SEARCH IN A HASHTABLE
+	
+	System.out.println("MAPPING = " +nodes_uuid.get(n.getHostAddress()) + " IP = "+n.getHostAddress());
 			
-			String key = null;
-	        String value = node.getUuid();
-	        for(Map.Entry entry: nodes_uuid.entrySet()){
-	            if(value.equals(entry.getValue())){
-	                key = entry.getKey().toString();
-	                break; //breaking because its one to one map
-	            }
-	        }
-	        System.out.println("Got key from value in hashtable key:  "+ key +" value: " + value);
-	 */
+	String key = null;
+    String value = node.getUuid();
+    for(Map.Entry entry: nodes_uuid.entrySet()){
+        if(value.equals(entry.getValue())){
+            key = entry.getKey().toString();
+            break; //breaking because its one to one map
+        }
+    }
+    System.out.println("Got key from value in hashtable key:  "+ key +" value: " + value);
+    */
 	
 
 }
